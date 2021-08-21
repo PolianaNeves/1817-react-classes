e42742f2f8028ca10ef7f89069096cedc0ed895a
PolianaNeves <poliana.ambrosio.campos@gmail.com>
2021-08-21 18:39:44 -0300

    obtendo valores do contexto

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,12 +2,33 @@ import React, { Component } from "react";
 import "./estilo.css";
 
 class FormularioCadastro extends Component {
+  constructor() {
+    super();
+    this.titulo = "";
+  }
+
+  handleMudancaDeTitulo(event) {
+    this.titulo = event.target.value;
+    console.log(this.titulo);
+  }
+
   render() {
     return (
       <form className="form-cadastro">
-        <input type="text" placeholder="Título" className="form-cadastro_input"/>
-        <textarea placeholder="Escreva sua nota..." className="form-cadastro_input" rows={15}/>
-        <button className="form-cadastro_input form-cadastro_submit">Criar Nota</button>
+        <input
+          type="text"
+          placeholder="Título"
+          className="form-cadastro_input"
+          onChange={this.handleMudancaDeTitulo.bind(this)}
+        />
+        <textarea
+          placeholder="Escreva sua nota..."
+          className="form-cadastro_input"
+          rows={15}
+        />
+        <button className="form-cadastro_input form-cadastro_submit">
+          Criar Nota
+        </button>
       </form>
     );
   }
